468af6a93d0e6a8e3560aaf9a80ee418ba6713fd
sushant <bhattaraisushant99@gmail.com>
2025-07-01 08:17:52 +0545

    Add program model and service for CRUD operations

--- src/models/program.model.js
+++ src/models/program.model.js
@@ -1,1 +1,51 @@
- 
+const mongoose = require('mongoose');
+
+const programSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 100
+  },
+  department: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 100
+  },
+  level: {
+    type: String,
+    required: true,
+    enum: ['undergraduate', 'postgraduate', 'professional'],
+    trim: true
+  },
+  duration: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 50
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 1000
+  },
+  prerequisites: [{
+    type: String,
+    trim: true,
+    maxlength: 100
+  }],
+  image: {
+    type: String,
+    trim: true
+  },
+  brochureUrl: {
+    type: String,
+    trim: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Program', programSchema); 
--- src/services/program.service.js
+++ src/services/program.service.js
@@ -1,1 +1,25 @@
- 
+const Program = require('../models/program.model');
+
+class ProgramService {
+  async getAllPrograms(query = {}) {
+    return Program.find(query);
+  }
+
+  async getProgramById(id) {
+    return Program.findById(id);
+  }
+
+  async createProgram(data) {
+    return Program.create(data);
+  }
+
+  async updateProgram(id, data) {
+    return Program.findByIdAndUpdate(id, data, { new: true });
+  }
+
+  async deleteProgram(id) {
+    return Program.findByIdAndDelete(id);
+  }
+}
+
+module.exports = new ProgramService(); 
