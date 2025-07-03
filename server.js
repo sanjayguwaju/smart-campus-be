c3e6f7f305e2451a096fbb6f87469ade7ae1e26a
sushant <bhattaraisushant99@gmail.com>
2025-07-03 23:04:21 +0545

    Update CORS configuration to allow requests from the new frontend URL

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ app.use(compression());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'https://smart-campus-fe.netlify.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
