85a2be90101fabd7e9a5adc9dad6fc77c4f67f2c
sanjayguwaju <sanjayguwaju@gmail.com>
2025-07-17 08:57:48 +0545

    feat: add new frontend URL to CORS configuration

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
-    'https://smart-campus-fe.netlify.app'
+    'https://smart-campus-fe.netlify.app',
+    'https://smart-campus-front.netlify.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
